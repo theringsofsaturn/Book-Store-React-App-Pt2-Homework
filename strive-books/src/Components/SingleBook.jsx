304f27b91d781e5ea99e86563579833747d9c16a
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-30 03:02:21 +0200

    finished homework
    adding border style while selecting a single book with states.
    Everything working correctly

--- strive-books/src/Components/SingleBook.jsx
+++ strive-books/src/Components/SingleBook.jsx
@@ -3,16 +3,26 @@ import Card from "react-bootstrap/Card";
 import fantasy from "../Data/fantasy.json";
 import { Button } from "react-bootstrap";
 
-const SingleBook = ({book}) => (
-  <Card style={{ width: "18rem" }}>
-    <Card.Img variant="top" src={book.img} />
-    <Card.Body>
-      <Card.Title>{book.title}</Card.Title>
-      <Card.Text>{book.category}</Card.Text>
-      <Button variant="primary">{book.price}</Button>
-    </Card.Body>
-  </Card>
-);
+class SingleBook extends React.Component {
+  state = {
+    selected: false,
+  };
+
+  render() {
+    return (
+      <Card
+        onClick={() => this.setState({ selected: !this.state.selected })}
+        style={{ border: this.state.selected ? "3px solid red" : "none" }}
+      >
+        <Card.Img variant="top" src={this.props.book.img} />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>{this.props.book.category}</Card.Text>
+          <Button variant="primary">{this.props.book.price}</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default SingleBook;
-
